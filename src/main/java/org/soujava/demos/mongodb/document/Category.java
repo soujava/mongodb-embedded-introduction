a6a8aa0dd05ed4554a5710fd4b836d3eb32ad612
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-16 17:59:03 +0000

    feat: remove code structure for live code session
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/mongodb/document/Category.java
+++
@@ -1,8 +0,0 @@
-package org.soujava.demos.mongodb.document;
-
-import jakarta.nosql.Column;
-import jakarta.nosql.Embeddable;
-
-@Embeddable(Embeddable.EmbeddableType.GROUPING)
-public record Category(@Column String name, @Column String description) {
-}
