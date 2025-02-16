95605f48e465cf864dad1cb5ddde071354ab7524
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-16 06:37:22 +0000

    feat: create product entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/mongodb/document/Product.java
+++ src/main/java/org/soujava/demos/mongodb/document/Product.java
@@ -1,4 +1,57 @@
 package org.soujava.demos.mongodb.document;
 
+import jakarta.json.bind.annotation.JsonbVisibility;
+import jakarta.nosql.Column;
+import jakarta.nosql.Convert;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+import org.eclipse.jnosql.databases.mongodb.mapping.ObjectIdConverter;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
 public class Product {
+
+    @Id
+    @Convert(ObjectIdConverter.class)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Manufacturer manufacturer;
+
+    @Column
+    private List<String> tags;
+
+    @Column
+    private Set<Category> categories;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", manufacturer=" + manufacturer +
+                ", tags=" + tags +
+                ", categories=" + categories +
+                '}';
+    }
 }
