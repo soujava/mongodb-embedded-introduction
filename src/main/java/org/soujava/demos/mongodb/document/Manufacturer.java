5a34716fc45da51725b48df8431b2433f3b72f06
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-16 06:37:05 +0000

    feat: create manufacturer entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/mongodb/document/Manufacturer.java
+++ src/main/java/org/soujava/demos/mongodb/document/Manufacturer.java
@@ -1,4 +1,10 @@
 package org.soujava.demos.mongodb.document;
 
-public record Manufacturer() {
+import jakarta.nosql.Column;
+import jakarta.nosql.Embeddable;
+
+
+@Embeddable(Embeddable.EmbeddableType.GROUPING)
+public record Manufacturer(@Column String name, @Column String address, @Column String contactNumber) {
+
 }
